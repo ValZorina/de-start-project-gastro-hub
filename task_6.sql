c8884fe553a1b898f66db8e43953bb121daea86c
ValZorina <valeriiafink@gmail.com>
2026-06-20 22:50:26 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,17 @@
 /*добавьте сюда запросы для решения задания 6*/
+BEGIN;
+
+SELECT restaurant_uuid
+FROM cafe.restaurants
+WHERE menu -> 'Кофе' ? 'Капучино'
+FOR UPDATE;
+
+UPDATE cafe.restaurants
+SET menu = jsonb_set(
+    menu,
+    '{Кофе,Капучино}',
+    ROUND((menu -> 'Кофе' ->> 'Капучино')::NUMERIC * 1.2)::TEXT::jsonb
+)
+WHERE menu -> 'Кофе' ? 'Капучино';
+
+COMMIT;
