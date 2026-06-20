c4eed9e2706f3df77c46d2b9590fb6203b375af8
ValZorina <valeriiafink@gmail.com>
2026-06-20 22:26:45 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,9 @@
 /*добавьте сюда запрос для решения задания 3*/
+SELECT
+	r.restaurant_name, 
+	COUNT(DISTINCT rmwd.manager_uuid) - 1 AS manager_changes
+FROM cafe.restaurants AS r
+JOIN cafe.restaurant_manager_work_dates AS rmwd USING(restaurant_uuid)
+GROUP BY r.restaurant_name
+ORDER BY manager_changes DESC 
+LIMIT 3; 
