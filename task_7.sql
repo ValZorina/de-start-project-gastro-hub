58bb7626a1217f8681b2debf8efa16e77874f025
ValZorina <valeriiafink@gmail.com>
2026-06-20 23:07:21 +0300

    Update task_7.sql

--- task_7.sql
+++ task_7.sql
@@ -1,1 +1,14 @@
 /*добавьте сюда запросы для решения задания 6*/
+LOCK TABLE cafe.managers IN SHARE MODE;
+
+ALTER TABLE cafe.managers ADD COLUMN manager_phone_upd VARCHAR[];
+
+UPDATE cafe.managers
+SET manager_phone_upd = ARRAY[
+    CONCAT('8-800-2500-', ROW_NUMBER() OVER (ORDER BY manager_name) + 99),
+    manager_phone
+];
+
+ALTER TABLE cafe.managers DROP COLUMN manager_phone;
+
+COMMIT;
